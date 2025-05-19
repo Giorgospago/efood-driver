d66470cd300b504f6de9c66a86b21f61debc681c
Pagonoudis <giorgospago23@gmail.com>
2025-05-19 21:34:10 +0300

    Add web app manifest with icon configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,29 @@
+/// <reference types="@capacitor-community/safe-area" />
+/// <reference types="@capacitor/status-bar" />
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.efood.driver.app',
-  appName: 'efood-driver',
-  webDir: 'dist/efood-driver/browser'
+  appName: 'Driver',
+  webDir: 'dist/efood-driver/browser',
+  server: {
+    // url: "http://192.168.64.1:4200",
+    cleartext: true
+  },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: true
+    },
+    SafeArea: {
+      enabled: true,
+      customColorsForSystemBars: true,
+      statusBarColor: '#000000',
+      statusBarContent: 'light',
+      navigationBarColor: '#000000',
+      navigationBarContent: 'light'
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { provideIcons } from '@ng-icons/core';
 import * as heroicons from '@ng-icons/heroicons/outline';
 import { Socket } from 'ngx-socket-io';
+import {SafeArea} from '@capacitor-community/safe-area';
+import { StatusBar } from '@capacitor/status-bar';
+import { setBackForwardNavigationGestures } from "capacitor-plugin-ios-webview-configurator";
+setBackForwardNavigationGestures(true);
+
+StatusBar.setOverlaysWebView({ overlay: true });
 
 @Component({
   selector: 'app-root',
@@ -13,5 +19,12 @@ import { Socket } from 'ngx-socket-io';
     provideIcons(heroicons)
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    SafeArea.enable({
+      config: {
+      }
+    });
+  }
 }
